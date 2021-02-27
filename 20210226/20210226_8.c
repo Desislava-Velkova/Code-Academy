22bd6250c7153ee4422f71de045b28afa8d1ee37
Desi <desi_8459@abv.bg>
2021-02-27 12:31:16 +0200

    Uploaded tasks for dinamyc allocated stack

--- 20210226/20210226_8.c
+++ 20210226/20210226_8.c
@@ -13,70 +13,77 @@ int top()
 int isFull(); int isEmpty(void)*/
 
 #include <stdio.h>
-
-int MAXSIZE = 16;
-int stack[16];
-int top = -1;
-
-int isempty(){
-    if(top == -1){
-        return 1;
-    } else{
-        return 0;
-    }
-}
-int isfull(){
-    if(top == MAXSIZE){
-        return 1;
-    }else {
-        return 0;
-    }
-}
-int peek(){
-    return stack[top];
-}
-int pop(){
-    int data;
-
-    if(!isempty()){
-        data = stack[top];
-        top = top - 1;
-        return data;
-    } else {
-        printf("Could not retrieve data, Stack is empty.\n");
-    }
-}
-int push(int data){
-    if(!isfull()) {
-        top = top + 1;
-        stack[top] = data;
-    } else{
-        printf("Could not insert data, Stack is full.\n");
-    }
-}
+#include <limits.h>
+#include <stdlib.h>
+ 
+typedef struct{
+    int top;
+    int* contents;
+}Stack;
+ 
 int input(void){
-    
+    unsigned capacity = 0;
+
+    printf("With what capacity you want to create stack ?\n");
+    scanf("%ud",&capacity);
+
+    return capacity;
+}
+Stack* createStack(unsigned capacity){
+
+    Stack* stack = (Stack*)malloc(sizeof(Stack));
+
+    if(NULL == stack){
+        printf("Memory not allocated!\n");
+        exit(1);
+    } else{
+        printf("Memory is successful allocated!\n");
+    }
+
+    stack->top = -1;
+    stack->contents = (int*)malloc(capacity * sizeof(int));
+
+    return stack;
+}
+ 
+int isFull(Stack* stack, unsigned capacity){
+    return stack->top == (capacity - 1);
+}
+int isEmpty(Stack* stack){
+    return stack->top == -1;
+}
+void push(Stack* stack, int item){
+    if (isFull(stack, item))
+        return;
+    stack->contents[++stack->top] = item;
+    printf("%d pushed to stack\n", item);
+}
+int pop(Stack* stack){
+    if (isEmpty(stack))
+        return INT_MIN;
+    return stack->contents[stack->top--];
+}
+int peek(Stack* stack){
+    if (isEmpty(stack))
+        return INT_MIN;
+    return stack->contents[stack->top];
 }
 int main(){
-    // push items on to the stack
-    push(3);
-    push(5);
-    push(9);
-    push(1);
-    push(12);
-    push(15);
+    unsigned capacity = input();
 
-    printf("Element at top of the stack: %d\n" ,peek());
-    printf("Elements: \n");
-
-    // print stack data
-    while(!isempty()) {
-        int data = pop();
-        printf("%d\n",data);
+    Stack* stack = createStack(capacity);
+    
+    for(int i =0; i< capacity; i++){
+        push(stack, (1+i));
     }
+     
+    printf("\n\n");
 
-    printf("Stack full: %s\n" , isfull()?"true":"false");
-    printf("Stack empty: %s\n" , isempty()?"true":"false");
+    for(int i =0; i< capacity; i++){
+        printf("%d popped from stack\n", pop(stack));
+    }
+ 
+    free(stack);
     
     return 0;
 }

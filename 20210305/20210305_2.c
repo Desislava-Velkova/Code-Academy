31409165a79a297efd898d5fe49d037c2a55ec8d
Desi <desi_8459@abv.bg>
2021-03-06 15:35:52 +0200

    Uploaded task from lesson 03.05.2021

--- 20210305/20210305_2.c
+++ 20210305/20210305_2.c
@@ -13,12 +13,13 @@
 този случай изпращането ще струва ... лева.
 Изход 2:
 За пратки с тегло ... грама обем ... и ... грама ...обем е по-добре да бъдат изпратени заедно. В
-този случай изпращането ще струва ... лева.*/
+този случай изпращането ще струва ... лева. */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct{
-    float weght, size;
+    float weight, size;
 }clientPakcage;
 
 int inputClientChoise(void);
@@ -26,15 +27,24 @@ void fillInfoPackage(clientPakcage* ptr, int num);
 float checkPrice_by_Weight(clientPakcage* num);
 float checkPrice_by_size(clientPakcage* num);
 clientPakcage* allocMem(int num);
-void compare(clientPakcage*ptr, int num);
+float calculateSeparatePrices(clientPakcage*ptr, int num);
+float calculateTogetherPrices(clientPakcage*ptr, int num);
+float* allocMemFloat(int num);
 
 int main(void){
+    float separateTotal = 0;
+    float togetherTotal = 0;
     int num = inputClientChoise(); /*takes number of packages from the user*/
     
     clientPakcage *ptr_package = allocMem(num); /*allocates memory for clients packages*/
 
     fillInfoPackage(ptr_package, num); /*fills the info for packages from the user*/
 
+    separateTotal = calculateSeparatePrices(ptr_package, num);
+
+    togetherTotal = calculateTogetherPrices(ptr_package, num);
+
+    printf("In this case, it is better to send them %s.\n", (separateTotal < togetherTotal) ? "separately" : "together" );
 
     free(ptr_package); /*free the allocated memory*/
 
@@ -55,50 +65,47 @@ int inputClientChoise(void){
     }while(num <= 0);
 }
 float checkPrice_by_Weight(clientPakcage* ptr){
-   if(ptr->weght <= 20){
-       return 0.46;
-   }else if(ptr->weght >= 21 && ptr->weght <= 50){
-       return 0.69;
-   }else if(ptr->weght >= 51 && ptr->weght <= 100){
-       return 1.02;
-   }else if(ptr->weght >= 101 && ptr->weght <= 200){
-       return 1.75;
-   }else if(ptr->weght >= 201 && ptr->weght <= 350){
-       return 2.13;
-   }else if(ptr->weght >= 351 && ptr->weght <= 500){
-       return 2.44;
-   }else if(ptr->weght >= 501 && ptr->weght <= 1000){
-       return 3.20;
-   }else if(ptr->weght >= 1001 && ptr->weght <= 2000){
-       return 4.27;
-   }else if(ptr->weght >= 2000 && ptr->weght <= 3000){
-       return 5.03;
-   }else{
-       return -1;
-   }
+
+    if(ptr->weight <= 20){
+        return 0.46;
+    }else if((ptr->weight >= 21) && (ptr->weight <= 50)){
+    }else if((ptr->weight >= 51) && (ptr->weight <= 100)){
+        return 1.02;
+    }else if((ptr->weight >= 101) && (ptr->weight <= 200)){
+        return 1.75;
+    }else if((ptr->weight >= 201) && (ptr->weight <= 350)){
+        return 2.13;
+    }else if((ptr->weight >= 351) && (ptr->weight <= 500)){
+        return 2.44;
+    }else if((ptr->weight >= 501) && (ptr->weight <= 1000)){
+        return 3.20;
+    }else if((ptr->weight >= 1001) && (ptr->weight <= 2000)){
+        return 4.27;
+    }else{
+        return 5.03;
+    }
 }
 float checkPrice_by_size(clientPakcage* ptr){
+
     if(ptr->size <= 10){
-       return 0.01;
-   }else if(ptr->size> 10 && ptr->size <= 50){
-       return 0.11;
-   }else if(ptr->size > 50 && ptr->size <= 100){
-       return 0.22;
-   }else if(ptr->size > 100 && ptr->size <= 150){
-       return 0.33;
-   }else if(ptr->size > 150 && ptr->size <= 250){
-       return 0.56;
-   }else if(ptr->size > 250 && ptr->size <= 400){
-       return 1.50;
-   }else if(ptr->size > 400 && ptr->size <= 500){
-       return 3.11;
-   }else if(ptr->size > 500 && ptr->size <= 600){
-       return 4.89;
-   }else if(ptr->size > 600){
-       return 5.79;
-   }else {
-       return -1;
-   }
+        return 0.01;
+    }else if((ptr->size> 10) && (ptr->size <= 50)){
+        return 0.11;
+    }else if((ptr->size > 50) && (ptr->size <= 100)){
+        return 0.22;
+    }else if((ptr->size > 100) && (ptr->size <= 150)){
+        return 0.33;
+    }else if((ptr->size > 150) && (ptr->size <= 250)){
+        return 0.56;
+    }else if((ptr->size > 250) && (ptr->size <= 400)){
+        return 1.50;
+    }else if((ptr->size > 400) && (ptr->size <= 500)){
+        return 3.11;
+    }else if((ptr->size > 500) && (ptr->size <= 600)){
+        return 4.89;
+    }else {
+        return 5.79;
+    }
 }
 clientPakcage* allocMem(int num){
     clientPakcage *ptr = NULL;
@@ -107,16 +114,71 @@ clientPakcage* allocMem(int num){
     if(NULL == ptr){ /*chek is it allocated successfully*/
         printf("Memory not allocated!\n");
         exit(1);
-    } else{
-        printf("Memory successfully allocated.\n");
-    }
+    } 
+
     return ptr; 
 }
 void fillInfoPackage(clientPakcage* ptr, int num){
     for(int i=0; i<num; i++){
-        printf("\nEnter size for package №%d\n",i+1);
-        scanf("%f", &(ptr+i)->size);
-        printf("Enter weight for package №%d\n",i+1);
-        scanf("%f", &(ptr+i)->weght);
+        do {
+            printf("\nEnter size (in cm) for package №%d\n",i+1);
+            scanf("%f", &(ptr+i)->size);
+        } while ((ptr+i)->size <= 0);
+        do {
+            printf("Enter weight (in grams) for package №%d\n",i+1);
+            scanf("%f", &(ptr+i)->weight);
+        } while ((ptr+i)->weight > 3000 || (ptr+i)->weight <= 0);
     }
+}
+float calculateSeparatePrices(clientPakcage*ptr, int num){
+    int i;
+    float *price_size_ptr = allocMemFloat(num);
+    float *price_weight_ptr = allocMemFloat(num);
+
+    float total_price_separate = 0;
+
+    for(i=0; i<num; i++, ptr++){
+        *(price_size_ptr + i) = checkPrice_by_size(ptr);
+        *(price_weight_ptr+i) = checkPrice_by_Weight(ptr);
+
+        printf("For package №%d: weight price is = %.2f\t size price is = %.2f\n",i, *(price_weight_ptr+i), *(price_size_ptr+i));
+        
+        total_price_separate += *(price_size_ptr + i) + *(price_weight_ptr+i);
+    }
+    printf("Total price of separate packages: %.2f\n", total_price_separate);
+
+    free(price_size_ptr);
+    free(price_weight_ptr);
+
+    return  total_price_separate;
+}
+
+float calculateTogetherPrices(clientPakcage*ptr, int num) {
+    float price_size, price_weight, total_price_together = 0;
+    clientPakcage packagesTogether = { 0.0, 0.0 };
+
+    for(int i=0; i<num; i++, ptr++){
+        packagesTogether.size += ptr->size;
+        packagesTogether.weight += ptr->weight;
+    }
+    price_size = checkPrice_by_size(&packagesTogether);
+    price_weight = checkPrice_by_Weight(&packagesTogether);
+    printf("If combined, the weight price is = %.2f and the size price is = %.2f\n", price_weight, price_size);
+
+    total_price_together = price_size + price_weight;
+    printf("Total price of the packages sent as one: %.2f\n", total_price_together);
+
+    return total_price_together;
+}
+
+float* allocMemFloat(int num){
+    float *ptr = NULL;
+    ptr = (float *)malloc(num * sizeof(float)); /*allocated memory num*4 bytes */
+    
+    if(NULL == ptr){ /*chek is it allocated successfully*/
+        printf("Memory not allocated!\n");
+        exit(1);
+    } 
+
+    return ptr; 
 }
--- 20210305/20210305_3.c
+++ 20210305/20210305_3.c
@@ -12,52 +12,53 @@
 н.м.р.) е ... ºC , то температурата на хижа Вихрен (1950м н.м.р.) е ... ºC.*/
 
 #include <stdio.h>
-int input(float *choice);
-float calcTempreture(float choice, int num);
+#define TEMPERATURE_DIFFERENCE ( ((float)(2918-1950)/100) * 0.5)
+int input(float *startingTemp);
+float calcTempreture(float startingTemp, int choice);
 
 int main(void){
-    float choice = 0;
+    float startingTemp = 0;
 
-    int num = input(&choice);  
+    int choice = input(&startingTemp);  
 
-    if(num == 1){
+    if(choice == 1){
         printf("If the atmospheric pressure is normal and the temperature of Vihren hut is %.2f\n\
-        then the temperature of Vihren peak is %.2f ºC\n", choice,calcTempreture(choice, num));
+        then the temperature of Vihren peak is %.2f ºC\n", startingTemp, calcTempreture(startingTemp, choice));
     } else{
         printf("If the atmospheric pressure is normal and the temperature of Vihren peak is %.2f\n\
-        then the temperature of Vihren hut is %.2f ºC\n", choice,calcTempreture(choice, num));
+        then the temperature of Vihren hut is %.2f ºC\n", startingTemp, calcTempreture(startingTemp, choice));
     }
    
     return 0;
 }
-int input(float *choice){ 
-    int num = 0;
+int input(float *startingTemp){ 
+    int choice = 0;
 
     /*asks the user for wich one he want to the tempreture */
     do{
         printf("\nEnter for wich one you want to chek the tempreture:\n");
         printf("\tPress 1 for peak Vihren\n");
-        printf("\tPress 2 for hut Vihren\n");
-        scanf("%d",&num);
+        printf("\tPress 2 for Vihren hut\n");
+        scanf("%d", &choice);
 
-        if(num < 1 || num > 2 ){
-            printf("Invalid choice! Please try again.\n");
+        if(choice < 1 || choice > 2 ){
+            printf("Invalid startingTemp! Please try again.\n");
         }
-    } while(num < 1 || num > 2 );
+    } while(choice < 1 || choice > 2 );
 
     /*asks the user about the temperature in the hut if he wants the temperature at the top and the opposite*/
-    (num == 1) ? printf("Enter the tempreture in hut Vihren.\n") : printf("Enter the tempreture in peak Vihren.\n");
-    scanf("%f",choice);
+    (choice == 1) ? printf("Enter the tempreture in hut Vihren.\n") : printf("Enter the tempreture in peak Vihren.\n");
+    scanf("%f", startingTemp);
 
-    return num;
+    return choice;
 }
-float calcTempreture(float choice, int num){
+float calcTempreture(float startingTemp, int choice){
     float tempreture = 0.0;
 
-    switch(num){
-        case 1: tempreture = choice-(((2918-1950)/100)*0,5);
+    switch(choice){
+        case 1: tempreture = startingTemp - TEMPERATURE_DIFFERENCE;
             break;
-        case 2: tempreture = choice+(((2918-1950)/100)*0,5);
+        case 2: tempreture = startingTemp + TEMPERATURE_DIFFERENCE;
             break;
     }
 

d4ac462193c501c82dc30877e9988f789e909b36
Desislava-Velkova <desi_8459@abv.bg>
2021-05-18 19:44:56 +0300

    adding function for barcode convert in binary data

--- Barcode_Scanner/include/FileParses.h
+++ Barcode_Scanner/include/FileParses.h
@@ -1,12 +1,14 @@
 #ifndef FILEPARSES_H
 #define FILEPARSES_H
 
-#include <errno.h>
 #include <stdio.h>
 
 FILE *fileOpen(char *fileName);
 int *AllocMatrix(int size);
 void assignCorectNumber(int number, int *bin);
 void inputMatrix(void);
+
+//TODO: izmestvane v .h na barcoda
+void assignThinThickerLine(int lenght, int *bina, int *counterLen, int *bin);
 
 #endif /*FILEPARSES_H*/
--- Barcode_Scanner/src/FileParses.c
+++ Barcode_Scanner/src/FileParses.c
@@ -3,12 +3,13 @@
 
 #include "FileParses.h"
 
-
-FILE *fileOpen(char *fileName){
+FILE *fileOpen(char *fileName)
+{
     FILE *fp = NULL;
     fp = fopen(fileName, "r+");
 
-    if(NULL == fp){
+    if (NULL == fp)
+    {
         perror("Can't open file ");
         exit(EXIT_FAILURE);
     }
@@ -16,11 +17,13 @@ FILE *fileOpen(char *fileName){
     return fp;
 }
 
-int *AllocMatrix(int size){
+int *AllocMatrix(int size)
+{
     int *iPtr = NULL;
-    iPtr = (int *) malloc (sizeof(int)* size);
+    iPtr = (int *)malloc(sizeof(int) * size);
 
-    if(NULL == iPtr){
+    if (NULL == iPtr)
+    {
         printf("Allocation memory failed!\n");
 
         exit(EXIT_FAILURE);
@@ -29,32 +32,82 @@ int *AllocMatrix(int size){
     return iPtr;
 }
 
-void assignCorectNumber(int number, int *bin){
-    if (number > 0.5) { 
+void assignCorectNumber(int number, int *bin)
+{
+    if (number > 0.5)
+    {
         *bin = 0;
-        printf("%d",*bin);  
     }
-    else {
+    else
+    {
         *bin = 1;
-        printf("%d",*bin);      
-    } 
+    }
 }
 
-void inputMatrix(void) {
+void assignThinThickerLine(int lenght, int *bina, int *counterLen, int *bin)
+{
+    for (int i = 0; i < lenght; i++)
+    {
+        if (bin[i] == 1 && bin[i + 1] == 1)
+        {
+            *bina = 1;
+            bina++;
+            i++;
+            //TODO: DA SE FIXNE NE PISHE V STOINOSTA, SAMO MESTI POITERA
+            *counterLen +=1;
+        }
+        else if (bin[i] == 1 && bin[i + 1] == 0)
+        {
+            *bina = 0;
+            bina++;
+            i++;
+            *counterLen+=1;
+        }
+    }
+}
+
+void inputMatrix(void)
+{
     float number = 0;
-	int lenght=0;
+    int lenght = 0;
+    int counterLen = 0;
     char *fileName = "test_cases\\Test1.txt";
 
     FILE *fp = fileOpen(fileName);
     fscanf(fp, "%d", &lenght);
 
     int *bin = AllocMatrix(lenght); /* creating array for storage data from the file */
+    int *bina = AllocMatrix(lenght);
+    int *ptr = bina;
 
-    for (int i = 0; i < lenght; i++) {
-        fscanf(fp, " %f ",&number); /* read's tha data from the file */
+    for (int i = 0; i < lenght; i++)
+    {
+        fscanf(fp, " %f ", &number); /* read's tha data from the file */
 
-		assignCorectNumber(number, &bin[i]); 
+        assignCorectNumber(number, &bin[i]);
+    }
+    //TODO: da se izmestqt v barcode faila
+    assignThinThickerLine(lenght, bina, &counterLen, bin);
+    printf("%d", counterLen);
+
+    for (int i = 0; i < counterLen; i++)
+    {
+        printf(" %d ", *(ptr + i));
     }
 
+    /* 
+    int size = 5;
+    int *resArray = AllocMatrix(size);
+   for (int i = 0; i < counterLen; i++)
+    {
+        if (*(ptr + i) == 0 && *(ptr + i + 1) == 0 && *(ptr + i + 2) == 1 && *(ptr + i + 3) == 1 && *(ptr + i + 4) == 0)
+        {
+            i = i + 5;
+
+            *resArray = realloc(resArray, size);
+            *resArray = realloc(resArray, size);
+        }
+    }*/
+    //TODO: da se izchsti alloca
     fclose(fp);
 }

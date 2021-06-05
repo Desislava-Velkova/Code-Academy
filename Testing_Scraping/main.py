07951d3905832562a8de79e336f90b1cff1df289
Desislava-Velkova <desi_8459@abv.bg>
2021-06-05 14:07:40 +0300

    Testing web scraping

--- Testing_Scraping/main.py
+++ Testing_Scraping/main.py
@@ -1,15 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL = 'https://www.jobs.bg/front_job_search.php?add_sh=1&categories%5B%5D=56'
+
+URL = 'https://igicheva.wordpress.com/all-posts/'
 
 html_text = requests.get(URL).text #takes the html data from the provide side
 
 soup = BeautifulSoup(html_text, 'lxml')  #make instance of the beaut.soup and put a parser 'lxml'
 
 
-job = soup.find('td', class_ ='offerslistRow').text
+blogs = soup.find_all('div', class_ ='entry-wrapper') #takes the content of the page
 
+index = 1
 
-company_name = job.find('a.card__title mdc-typography mdc-typography--headline6 text-overflow')
-print(company_name)
+for blog in blogs:
+    if index <= 20:
+        with open('output.txt', 'a') as file:
+            published_date = blog.find('time', class_='entry-date published').text
+            title = blog.find('h2', class_='entry-title').text
+            content = blog.find('p', class_='').text
+            
+            file.write(f'\n№: {index}\n')
+            file.write(f'Title: {title}\n')
+            file.write(f'Content: {content}\n')
+            file.write(f'|Date: {published_date}\n')
+            
+        index =index + 1
+    else:
+        break
+    
